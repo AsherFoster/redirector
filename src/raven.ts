b250660ec1c3f51e87c2aec92130139343a20463
Asher Foster <me@asherfoster.com>
2018-10-07 01:09:35 +1300

    Why the fuck was that working in the first place?

--- src/raven.ts
+++ src/raven.ts
@@ -1,10 +1,9 @@
 import * as Raven from 'raven';
-import config from './config';
 
 const RAVEN_DSN = process.env.CUSTOMCONNSTR_RAVEN_DSN || process.env.RAVEN_DSN;
 const ENVIRONMENT = process.env.NODE_ENV || 'production';
 
-Raven.config(ENVIRONMENT === 'production' && config.sentry.dsn, {
+Raven.config(ENVIRONMENT === 'production' && RAVEN_DSN, {
   autoBreadcrumbs: true,
   captureUnhandledRejections: true
 }).install();
