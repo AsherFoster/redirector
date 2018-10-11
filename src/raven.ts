b052b83839b3a39d83f7c809ea5b168c5423fb45
Asher Foster <me@asherfoster.com>
2018-10-11 19:00:22 +1300

    Negating NODE_ENV=production causing devDeps to not be installed
    Signed-off-by: Asher Foster <me@asherfoster.com>

--- src/raven.ts
+++ src/raven.ts
@@ -2,7 +2,7 @@ import * as Raven from 'raven';
 import {spawnSync} from 'child_process';
 const SENTRY_DSN = process.env.CUSTOMCONNSTR_SENTRY_DSN || process.env.SENTRY_DSN;
 const ENVIRONMENT = process.env.NODE_ENV || 'production';
-const RELEASE = spawnSync('./node_modules/.bin/sentry-cli', ['releases', 'propose-version']).stdout.toString().slice(0, -1);
+const RELEASE = spawnSync('git', ['rev-parse', 'HEAD']).stdout.toString().slice(0, -1);
 
 Raven.config(ENVIRONMENT === 'production' && SENTRY_DSN, {
   autoBreadcrumbs: true,
